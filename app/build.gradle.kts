dd489141295876594b1201b9536f1c8c1ad6c7d4
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-04-28 10:46:14 +0200

    Update versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,12 +17,12 @@ plugins {
 
 android {
     namespace = "com.flash.climora"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.flash.climora"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
